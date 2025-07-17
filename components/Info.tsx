e5995c37b3637dc53d7979b0626911e87d3f2f15
Niloy Chowdhury <iamvrniloyc@gmail.com>
2025-07-17 16:06:42 +0530

    Fixed responsiveness for mobile devices

--- components/Info.tsx
+++ components/Info.tsx
@@ -7,11 +7,11 @@ const Info = () => {
         className=" 
         bg-clip-text text-transparent 
         [background-image:radial-gradient(circle_at_center,_rgb(255,_211,_127)_0%,_rgb(232,_65,_123)_50%,_rgb(112,_30,_101)_80%,_rgba(32,_31,_66,_0)_100%)] 
-        bg-fixed bg-cover bg-no-repeat  w-[65%]
+        bg-fixed bg-cover bg-no-repeat w-[80%] lg:w-[65%]
       "
       >
-        <h1 className="text-5xl font-extrabold">Madhyamgram, WB.</h1>
-        <p className="text-2xl leading-normal pt-5 font-extrabold">
+        <h1 className="text-xl text-center md:text-4xl lg:text-left lg:text-5xl font-extrabold">Madhyamgram, WB.</h1>
+        <p className="text-sm md:text-xl text-center lg:text-left lg:text-2xl leading-normal pt-5 font-extrabold">
           I accidentally spilled coffee on my keyboard one night, and instead of
           frying it, the caffeine somehow seeped into my code and sparked
           something wild — a website so alive with GSAP animations, it felt like
