6bea6cfd7e1cd2686e5d0c623e26c8f3013941bc
Niloy Chowdhury <iamvrniloyc@gmail.com>
2025-07-22 12:35:44 +0530

    Added PreLoading Screen

---
+++ components/Preloader.tsx
@@ -0,0 +1,73 @@
+"use client";
+import React, { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(SplitText);
+
+const Preloader = ({
+  setIsLoading,
+}: {
+  setIsLoading: (state: boolean) => void;
+}) => {
+  const textRef = useRef(null);
+  const containerRef = useRef(null);
+
+  useGSAP(() => {
+    let split;
+
+    // First hide the text
+    gsap.set(textRef.current, { opacity: 0 });
+
+    SplitText.create(textRef.current, {
+      type: "chars",
+      charsClass: "char",
+      onSplit: (self) => {
+        // Then animate characters in
+        split = gsap.fromTo(
+          self.chars,
+          { yPercent: 100, opacity: 0 },
+          {
+            yPercent: 0,
+            opacity: 1,
+            stagger: 0.1,
+            duration: 1,
+            delay: 0.7,
+            ease: "expo.out",
+            onStart: () => {
+              gsap.set(textRef.current, { opacity: 1 }); // show parent wrapper when animation starts
+            },
+            onComplete: () => {
+              // Slide the entire screen up
+              gsap.to(containerRef.current, {
+                yPercent: -100,
+                duration: 1,
+                ease: "power3.inOut",
+                delay: 0.3,
+                onComplete: () => setIsLoading(false),
+              });
+            },
+          }
+        );
+        return split;
+      },
+    });
+  }, []);
+
+  return (
+    <div
+      ref={containerRef}
+      className="h-screen absolute left-0 top-0 flex justify-center items-center w-full bg-gradient-to-r from-fuchsia-600 to-pink-600 z-[999999] text-white overflow-hidden"
+    >
+      <h1
+        ref={textRef}
+        className="text-[6rem] md:text-[11rem] lg:text-[16rem] font-extrabold uppercase leading-none opacity-0"
+      >
+        NILOY
+      </h1>
+    </div>
+  );
+};
+
+export default Preloader;
--- components/Provider.tsx
+++ components/Provider.tsx
@@ -1,21 +1,28 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Lenis from "lenis";
+import Preloader from "./Preloader";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
+
+  const [isLoading,setIsLoading] = useState(true)
+
   useEffect(() => {
     const lenis = new Lenis({
       duration: 2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
     });
-    function raf(time: DOMHighResTimeStamp) {
+    function raf(time: DOMHighResTimeStamp) 
+    {
       lenis.raf(time);
       requestAnimationFrame(raf);
     }
     requestAnimationFrame(raf);
   }, []);
 
-  return <div>{children}</div>;
+  return <div>
+    {isLoading?<Preloader setIsLoading={setIsLoading}/>:null}
+    {children}</div>;
 };
 
 export default Provider;
